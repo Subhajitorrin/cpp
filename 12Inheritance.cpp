6758e80bc17a18b21bc86caa9b6ed08de4e22de0
subhajitghosh_ <109899216+Subhajitorrin@users.noreply.github.com>
2023-04-21 11:24:18 +0530

    Update 12Inheritance.cpp

--- 12Inheritance.cpp
+++ 12Inheritance.cpp
@@ -1,4 +1,10 @@
 /*Inheritance*/
+/*
+Inheritance is a way of creating new classes based on existing classes. 
+The new class inherits the properties and behaviors of the existing class, 
+and can add new features or modify the inherited ones.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 class a
